078289c589eeef29f7c9c20fc8e69cffa39eb8b4
JayM647 <jemarmrrs@gmail.com>
2024-04-02 06:45:46 -0400

    updated project with corrected links, html and css

--- server.py
+++ server.py
@@ -4,21 +4,31 @@ app = Flask(__name__)
 print(__name__)
 
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template('index.html', name=username, post_id=post_id)
+@app.route("/")
+def my_home():
+    return render_template('index.html')
 
 
-@app.route("/about.html")
-def about():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route("/blog")
-def blog():
-    return "These are my thoughts on code"
+# @app.route("/work.html")
+# def work():
+#     return render_template('work.html')
 
 
-@app.route("/blog/2020/dogs")
-def blog2():
-    return "This is my dog"
+# @app.route("/works.html")
+# def works():
+#     return render_template('works.html')
+
+
+# @app.route("/about.html")
+# def about():
+#     return render_template('about.html')
+
+
+# @app.route("/contact.html")
+# def contact():
+#     return render_template('contact.html')
